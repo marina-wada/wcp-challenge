82c27faa1eb49ef9362af4b8c52d53a8d2821b79
marina <wdmrna4i1@gmail.com>
2022-06-03 08:09:38 +0900

    [Finish]Javascript_chapter6

--- challenge-1/jQuery-Lesson/app.js
+++ challenge-1/jQuery-Lesson/app.js
@@ -26,7 +26,7 @@ $(function(){
 $(function(){
   $('.box1').hide();
 });
-*/
+
 $(function(){
   $('.box1').slideDown(function(){
     $('.box1').css({
@@ -34,5 +34,32 @@ $(function(){
       'width':'200px',
       'height':'100px'
     }).slideUp(5000);
+  });
+});
+
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color':'#0000FF'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color':'#FF0000'});
+  });
+});
+
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+});
+*/
+$(function(){
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
